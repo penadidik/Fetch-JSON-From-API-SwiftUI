6eda769b8d0c6db5ed8864965df1260e004b5122
didik <didik.maryono@pawoon.com>
2022-04-23 00:17:29 +0700

    Initial Commit

---
+++ FetchJSONFromApi/FetchJSONFromApiApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchJSONFromApiApp.swift
+//  FetchJSONFromApi
+//
+//  Created by Didik Maryono on 23/04/22.
+//
+
+import SwiftUI
+
+@main
+struct FetchJSONFromApiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
